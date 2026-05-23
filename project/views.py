23a849d261acaeb22a56fb36a5783c3f1500f046
Omar Thompson <omarshakkaithompson@gmail.com>
2026-05-23 14:51:44 +1000

    Created the extra routes for website traversal

--- project/views.py
+++ project/views.py
@@ -5,3 +5,19 @@ from project import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/bookmarks')
+def bookmarks():
+    return render_template("bookmarks.html")
+
+@app.route('/listing')
+def listing():
+    return render_template("listing.html")
+
+@app.route('/person_details')
+def person_details():
+    return render_template("person_details.html")
+
+@app.route('/property_details')
+def property_details():
+    return render_template("property_details.html") 
